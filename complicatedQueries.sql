72eb437ee7c2c284454d2136202b798e2cb60185
Dmitriy Korotkov <nupellot@yahoo.com>
2022-03-31 23:19:31 +0300

    Исправлен 3 сложный запрос

--- complicatedQueries.sql
+++ complicatedQueries.sql
@@ -1,3 +1,5 @@
+use scheme_1;
+
 # 1. Создать отчёт о лечащих врачах отделения XXX за 2020 год по форме: Уникальный номер врача, Фамилия врача, количество пациентов, у которых врач был лечащим. 
 SELECT id_doctor, inits, surname, count(id_story) AS 'AMOUNT OF PATIENTS' FROM department JOIN doctor ON id_department = doctors_department JOIN story ON id_doctor = story_doctor
 WHERE department.department_name = 'Травмпункт' AND (year(reg_date) = 2020 OR year(discharge_date) = 2020)
@@ -17,9 +19,9 @@ WHERE dismissal_date IS NULL
 ORDER BY current_date() - recruitment_date DESC
 LIMIT 1;
 
-SELECT id_doctor, current_date() - recruitment_date  # НЕ РАБОТАЕТ
+SELECT doctor.*, DATEDIFF(current_date(), recruitment_date) AS 'Проработал дней'  # НЕ РАБОТАЕТ
 FROM doctor
-WHERE dismissal_date IS NULL AND recruitment_date = (SELECT MIN(recruitment_date) FROM doctor);
+WHERE recruitment_date = (SELECT MIN(recruitment_date) FROM doctor WHERE dismissal_date IS NULL);
 
 
 # 4. Показать все сведения о врачах, которые ни разу не назначались лечащими врачами у пациентов.
