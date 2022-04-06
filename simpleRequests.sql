c126c5f0bb165253faa5bf31dc186f75f55e6a79
Dmitriy Korotkov <nupellot@yahoo.com>
2022-04-06 21:29:28 +0300

    Причесаны простые запросы

--- simpleRequests.sql
+++ simpleRequests.sql
@@ -1,25 +1,31 @@
-use scheme_1;
+# Простые запросы
+USE scheme_1;
 show tables;
 
-select * from doctor;
-/* Показать все сведения о врачах, принятых на работу в апреле 2013 года */
-SELECT * FROM doctor WHERE year(recruitment_date) = 2013 and month(recruitment_date) = 04;
-/* Показать все сведения о пациентах, выписавшихся из гостпиталя за последние 20 дней */
-select distinct * from patient, story where datediff(current_date(), story.discharge_date) <= 20 and story.story_patient = patient.id_patient;
+SELECT * FROM doctor;
+/* 1. Показать все сведения о врачах, принятых на работу в апреле 2013 года */
+SELECT * FROM doctor
+WHERE year(recruitment_date) = 2013 AND month(recruitment_date) = 04;
 
-/* Показать, сколькими палатами каждой категории располагает каждое отделение госпиталя */
-select wards_department, wtype, count(wtype) from ward group by wards_department, wtype;
+/* 2. Показать все сведения о пациентах, выписавшихся из гостпиталя за последние 20 дней */
+SELECT DISTINCT * FROM patient, story 
+WHERE datediff(current_date(), story.discharge_date) <= 20 AND story.story_patient = patient.id_patient;
 
-/* Показать средний возраст пациентов каждого отделения */
-select id_department, (sum(datediff(current_date(), patient.birth_date))/count(patient.birth_date)) as average from department, ward, story, patient where (id_department = wards_department) and (id_ward = story_ward) and (id_patient = story_patient) group by id_department;
-select id_department, (sum(datediff(current_date(), patient.birth_date))/count(patient.birth_date))/365 as average from department, ward, story, patient where (id_department = wards_department) and (id_ward = story_ward) and (id_patient = story_patient) group by id_department;
+/* 3. Показать, сколькими палатами каждой категории располагает каждое отделение госпиталя */
+SELECT wards_department, wtype, count(wtype) FROM ward 
+GROUP BY wards_department, wtype;
 
-/* Показать, сколько пациентов из Москвы находится в данный момент в госпитале */
-select count(id_patient) from patient, story 
-where patient.id_patient = story.story_patient 
-and address = 'Москва'
-and discharge_date IS NULL;
+/* 4. Показать средний возраст пациентов каждого отделения */
+SELECT id_department, (sum(datediff(current_date(), patient.birth_date))/count(patient.birth_date))/365 AS average FROM department, ward, story, patient 
+WHERE (id_department = wards_department) AND (id_ward = story_ward) AND (id_patient = story_patient) 
+GROUP BY id_department;
 
-/* Показать, сколько записей в истории болезней сделал каждый приглашенный консультант в 2020 году */
-select id_doctor, count(id_survey) as amount from doctor, survey 
-where (doctor.id_doctor = survey.survey_doctor) group by id_doctor;
+/* 5. Показать, сколько пациентов из Москвы находится в данный момент в госпитале */
+SELECT count(id_patient) FROM patient, story 
+WHERE patient.id_patient = story.story_patient 
+AND address = 'Москва'
+AND discharge_date IS NULL;
+
+/* 6. Показать, сколько записей в истории болезней сделал каждый приглашенный консультант в 2020 году */
+SELECT id_doctor, count(id_survey) AS amount FROM doctor, survey 
+WHERE (doctor.id_doctor = survey.survey_doctor) GROUP BY id_doctor;
